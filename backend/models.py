6b773a34ee0f82a1a43a6dbfb934033a1abfa80e
Niklas Rousset <niklas.rousset@gmail.com>
2024-02-26 19:21:53 +0100

    More detailed models

--- backend/models.py
+++ backend/models.py
@@ -1,30 +1,63 @@
+from enum import StrEnum
+
 from pydantic import BaseModel
+
+
+class UnitEnum(StrEnum):
+    g = "g"
+    kg = "kg"
+    ml = "ml"
+    l = "l"
+    pcs = "pcs"
+
+
+class Ingredient(BaseModel):
+    """An ingredient model."""
+
+    name: str
+    unit: UnitEnum
+    amount: float
+
+
+class Step(BaseModel):
+    """A step model."""
+
+    instructions: str
 
 
 class RecipeBase(BaseModel):
     """A recipe model."""
 
-    # TODO: add rating
     # TODO: add user/creator
 
     title: str
     description: str
-    # TODO: add ingredient model with name, unit and amount
-    ingredients: list[str]
+    portions: int
+    ingredients: list[Ingredient]
     steps: list[str]
     categories: list[str]
 
 
 class RecipeStored(RecipeBase):
-    """A recipe model."""
+    """A recipe model with an id."""
 
     id_: int = -1
 
 
 class Recipe(RecipeStored):
-    """A recipe model."""
+    """A complete recipe model with images."""
 
     images: list[int]
+
+
+class RecipeListing(BaseModel):
+    """A slimmer recipe model for listings."""
+
+    id_: int = -1
+    title: str
+    description: str
+    cover_image: int
+    rating: float
 
 
 class ImageBase(BaseModel):
