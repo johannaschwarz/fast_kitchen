3c22b532015905f4aa6b9d15dc5a060dd58b74f8
Niklas Rousset <niklas.rousset@gmail.com>
2024-04-14 17:45:39 +0200

    Adds version to footer

--- frontend/src/Config.js
+++ frontend/src/Config.js
@@ -1,1 +1,2 @@
 export const API_BASE = 'http://localhost:8000/';
+export const FASTKITCHEN_VERSION = "v0.1.0";
--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -1,16 +1,20 @@
 import { Stack } from "@mui/material";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { FASTKITCHEN_VERSION } from "./Config";
 import { AuthContext } from "./index";
 
 function Footer() {
     const { loggedIn, logout, isAdmin } = useContext(AuthContext);
     return (
         <footer>
-            <Stack direction={"row"} spacing={2} justifyContent={"center"}>
-                {loggedIn && <span className="link" onClick={logout}>Log out</span>}
-                {(loggedIn && isAdmin) && <Link className="link" to="/register">Create user</Link>}
-                <Link className="link" to="/legalnotice">Legal notice</Link>
+            <Stack direction={"column"} spacing={2}>
+                <Stack direction={"row"} spacing={2} justifyContent={"center"}>
+                    {loggedIn && <span className="link" onClick={logout}>Log out</span>}
+                    {(loggedIn && isAdmin) && <Link className="link" to="/register">Create user</Link>}
+                    <Link className="link" to="/legalnotice">Legal notice</Link>
+                </Stack>
+                <span>© 2024 FastKitchen {FASTKITCHEN_VERSION}</span>
             </Stack>
         </footer>
     )
