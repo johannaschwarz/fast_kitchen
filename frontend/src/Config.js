6203d3c9027760d439a122981beedd5d05daf33e
johannaschwarz <johanna@schwarzweb.de>
2024-04-15 20:38:19 +0200

    Update API_BASE URL in Config.js

--- frontend/src/Config.js
+++ frontend/src/Config.js
@@ -1,3 +1,3 @@
-export const API_BASE = 'https://fastkitchen.ey.r.appspot.com/';
+export const API_BASE = 'http://87.106.167.231/';
 //export const API_BASE = 'http://localhost:8000/';
 export const FASTKITCHEN_VERSION = "v0.1.2";
