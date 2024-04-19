1753e98fe981f05ed064d692cb300db6b5aa945b
Niklas Rousset <niklas.rousset@gmail.com>
2024-04-19 15:32:48 +0200

    version updates

--- frontend/src/Config.js
+++ frontend/src/Config.js
@@ -1,3 +1,3 @@
 export const API_BASE = 'https://api.flottekueche.de/';
 //export const API_BASE = 'http://localhost:8000/';
-export const FASTKITCHEN_VERSION = "v0.2.0";
+export const FASTKITCHEN_VERSION = "v0.2.1";
