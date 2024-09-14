2de554a223f13c1576e5c99a3e0e0e2265c2d5e5
johannaschwarz <johanna@schwarzweb.de>
2024-09-14 12:56:24 +0200

    Enable Recipe sorting

--- frontend/src/Recipes.js
+++ frontend/src/Recipes.js
@@ -4,16 +4,50 @@ import { ThreeDots } from 'react-loader-spinner';
 import { Link } from "react-router-dom";
 import { API_BASE } from './Config';
 import RecipeCard from './RecipeCard';
-
 import './Recipes.css';
+
+import { ToggleButton } from '@mui/material';
+import { ArrowUpward, ArrowDownward } from '@mui/icons-material';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Stack from '@mui/material/Stack';
 
 function Recipes({ filters, search }) {
     const [recipes, setRecipies] = useState(null)
+    const [sortByOption, setSortByOption] = useState('Clicks');
+    const [sortOrder, setSortOrder] = useState('DESC');
+
+    const fetchSortedRecipes = async (sortByOption, sortOrder) => {
+        setRecipies(null);
+        try {
+            let params = new URLSearchParams()
+            params.append("sort_by", sortByOption)
+            params.append("sort_order", sortOrder)
+
+            const response = await fetch(API_BASE + 'recipe/all?' + new URLSearchParams(params));
+            const data = await response.json();
+            setRecipies(data);
+        } catch (error) {
+            console.error('Error fetching sorted recipes:', error);
+        }
+    }
+
+    const handleSortByChange = (e) => {
+        setSortByOption(e.target.value);
+    };
+
+    const toggleSortOrder = () => {
+        setSortOrder((prevOrder) => prevOrder === 'ASC' ? 'DESC' : 'ASC');
+    }
 
     useEffect(() => {
         if ((filters != null && filters.length > 0) || (search != null && search.length > 0)) {
             setRecipies(null)
             let params = new URLSearchParams()
+            params.append("sort_by", sortByOption)
+            params.append("sort_order", sortOrder)
 
             if (filters != null && filters.length > 0) {
                 filters.map((filter) => params.append("categories", filter))
@@ -31,20 +65,40 @@ function Recipes({ filters, search }) {
                     console.log(err.message)
                 });
         } else {
-            setRecipies(null);
-            fetch(API_BASE + 'recipe/all')
-                .then((response) => response.json())
-                .then((data) => {
-                    setRecipies(data)
-                })
-                .catch((err) => {
-                    console.log(err.message)
-                });
+            fetchSortedRecipes(sortByOption, sortOrder);
         }
-    }, [filters, search]);
+    }, [filters, search, sortByOption, sortOrder]);
 
     return (
         <div>
+            <Stack
+                direction="row"
+                spacing={2}
+                sx={{
+                    justifyContent: "flex-end",
+                    alignItems: "center",
+                }}>
+                <FormControl>
+                    <InputLabel id="demo-simple-select-label">Sorting</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={sortByOption}
+                        label="Sorting"
+                        onChange={handleSortByChange}
+                    >
+                        <MenuItem value={"Clicks"}>Clicks</MenuItem>
+                        <MenuItem value={"RecipeID"}>Date Created</MenuItem>
+                        <MenuItem value={"Title"}>Title</MenuItem>
+                        <MenuItem value={"CookingTime"}>Cooking Time</MenuItem>
+                    </Select>
+                </FormControl>
+
+                <ToggleButton onClick={toggleSortOrder}>
+                    {sortOrder === 'ASC' ? <ArrowDownward /> : <ArrowUpward />}
+                </ToggleButton>
+            </Stack>
+
             <section id="recipes">
                 {recipes != null && recipes.length > 0 && recipes.map((recipe, index) => (
                     <Link key={index} to={'recipe/' + recipe.id_}><RecipeCard recipe={recipe} /></Link>
