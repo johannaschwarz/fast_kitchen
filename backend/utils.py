60dd22d0225364cccf96e8fa514fe71ac8ad177a
Niklas Rousset <niklas.rousset@gmail.com>
2025-03-28 15:22:00 +0100

    make config and credentials cached

--- backend/utils.py
+++ backend/utils.py
@@ -1,16 +1,19 @@
 import json
+from functools import cache
 
 from PIL import Image as PILImage
 
 SIZE = (700, 700)
 
 
+@cache
 def load_config() -> dict:
     """Load the configuration from the config.json file."""
     with open("assets/config.json", encoding="utf-8") as f:
         return json.load(f)
 
 
+@cache
 def load_credentials() -> dict:
     """Load the credentials from the creds.json file."""
     with open("assets/creds.json", encoding="utf-8") as f:
